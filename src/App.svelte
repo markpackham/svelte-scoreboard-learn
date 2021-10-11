e59cd1a19a025d226d0ea4ba2e6b87c9a18f39b0
mark <markpackham1@gmail.com>
2021-10-11 13:23:44 +0100

    add player
    https://youtu.be/uK2RnIzrQ0M?t=1586

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navbar from './Navbar.svelte'
 	import Player from './Player.svelte'
+	import AddPlayer from './AddPlayer.svelte'
 
 
 	let players = [
@@ -18,11 +19,16 @@
     }
   ];
 
+	const addPlayer = (e) =>{
+		const newPlayer = e.detail;
+		players = [...players, newPlayer];
+	}
 </script>
 
 
 <Navbar />
 <div class="container">
+	<AddPlayer on:addplayer={addPlayer} />
 	{#if players.length === 0}
 	<p>No players</p>
 	{:else}
